bcb38bc8600530a8327451f129d66c9d578905a5
程晓玲 <chengxiaoling@rong-yun.com>
2019-05-14 14:37:53 +0800

    提交项目

---
+++ XLPhotoBrowser/XLImageCollectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  XLImageCollectionCell.swift
+//  XLPhotoBrowser
+//
+//  Created by kayling on 2019/4/28.
+//  Copyright © 2019年 Kayling. All rights reserved.
+//
+
+import UIKit
+
+class XLImageCollectionCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
